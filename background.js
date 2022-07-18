43a93793436b07e8d3a136c46b1afdbb2cd5b2e6
Aosmiles <arlin.smiles@gmail.com>
2022-07-18 16:10:14 -0700

    add background.js

---
+++ background.js
@@ -0,0 +1,9 @@
+﻿
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if(tab.url && tab.url.includes('fonts.google.com') && changeInfo.status === 'complete'){
+    console.log('google fonts loaded')
+    chrome.tabs.sendMessage(tabId, {
+      loaded: true
+    })
+  }
+})
