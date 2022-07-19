c9e95570abe944b3cd601a49b1b915327a2b5d55
Aosmiles <arlin.smiles@gmail.com>
2022-07-18 21:11:34 -0700

    add working copy button to DOM

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 ﻿
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if(tab.url && tab.url.includes('fonts.google.com') && changeInfo.status === 'complete'){
-    console.log('google fonts loaded')
+    // console.log('google fonts loaded')
     chrome.tabs.sendMessage(tabId, {
       loaded: true
     })
--- script.js
+++ script.js
@@ -1,65 +1,34 @@
-﻿console.log("this is google fonts website")
+﻿
+let alreadyLoaded = false;
 
-const extension = function () {
-  const observerConfig = { childList: true }
-  const copyButton = document.createElement('button');
-  copyButton.innerText = 'copy';
-  
-  function onDrawerToggled(mutationList) {
-    for(const mutation of mutationList) {
-      console.log("mutation")
-    }
-    // const embedCode = document.querySelector('.embed-code__link')
-    // console.log(embedCode);
-    // if(embedCode){
-    //   const parent = embedCode.parentElement;
-    //   console.log("appended button")
-    //   parent.append(copyButton);
-    // }
+function createButton() {
+  const embedCodeNode = document.querySelector('.embed-code__link')
+  if(embedCodeNode){
+    const button = document.createElement('button');
+    button.innerText = 'copy';
+    button.style.marginTop = '10px'
+    button.addEventListener('click', () => {
+      navigator.clipboard.writeText(embedCodeNode.innerText).then(() => {
+        console.log("copied!")
+      })
+    })
+    embedCodeNode.parentElement.append(button)
   }
-  
-  function addButton() {
-    
-  }
+}
 
-  function run () {
-    const targetNode = document.querySelector('.collection-drawer-layout-container')
-    console.log(targetNode !== null ? 'target found': 'target is null')
-    
-    const observer = new MutationObserver(onDrawerToggled)
-    observer.observe(targetNode, observerConfig);
-  }
+function run(){
+  alreadyLoaded = true;
   
-  return {run}
-}();
+  const shoppingBagButton = document.querySelector('gf-shopping-bag')
+  shoppingBagButton.addEventListener('click', () => {
+    createButton()
+  })
+  createButton();
+}
 
 chrome.runtime.onMessage.addListener((obj, sender, response) => {
   const { loaded } = obj;
-  if (loaded){
-    extension.run();
+  if (loaded && !alreadyLoaded){
+    run()
   }
 })
-
-
-
-
-
-
-// const observer = new MutationObserver(function() {
-//   const embedCode = targetNode.querySelector('.embed-code__link');
-//   const parent = embedCode.parentElement;
-//
-//   function copyLink () {
-//     navigator.clipboard.writeText(embedCode.innerText).then(() => {
-//       console.log("copied!")
-//     })
-//   }
-//
-//   const button = document.createElement('button');
-//   button.innerText = 'copy';
-//   button.addEventListener('click', copyLink);
-//
-//   parent.append(button);
-// })
-//
-// observer.observe(targetNode, observerConfig)
